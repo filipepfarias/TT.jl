d6312a63cf00495863d3c1c48225855594bf5605
Filipe P. Farias <filipepfarias@gmail.com>
2024-03-10 12:23:10 -0300

    Tests initiated.

--- src/TT.jl
+++ src/TT.jl
@@ -7,8 +7,6 @@ include("TTTensor.jl")
 export TTTensor, qtt_x, tt_ones, rank, ranks, rounded_diagonal_index, size, mem,
 tt_ones,+,-,*,norm,core_to_vector,dot,TTrand
 
-include("solve.jl")
-
 include("cross.jl")
 export funcrs, maxvol2, rounded_diagonal_index, lu_full, reort
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using TT
 using Test
 
 @testset "TT.jl" begin
-    # Write your tests here.
+
 end
